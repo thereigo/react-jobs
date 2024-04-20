5fa1c108f94204769a5edb5e6abb7f6ef7dde670
Igors Pozdnakovs <ig.pozdnakovs@gmail.com>
2024-04-20 23:36:26 +0300

    Update vite.config.js, dist/index.html, package.json, and dist/vite.svg

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react-build/',
   server: {
     port: 3000,
     proxy: {
